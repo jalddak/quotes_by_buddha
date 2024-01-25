cfb8b52b4391ec8661e7bf34aeb268af469d0ab1
jalddak <jalddak98@gmail.com>
2024-01-25 16:56:19 +0900

    create

---
+++ src/main/java/toy/buddha/BuddhaApplication.java
@@ -0,0 +1,13 @@
+package toy.buddha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toy/buddha/BuddhaApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.buddha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuddhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
